5656724924b079f81333f19c9b791127304522ed
Trrn13P <tor-andreas_93@hotmail.com>
2020-11-07 17:19:48 +0100

    loorran

--- main.cpp
+++ main.cpp
@@ -21,11 +21,10 @@ and else returns i, for PBC.
 */
 int PBC_index(int i, int L){
   return (L-(L-i%L)%L)%L+(L-i-(L-i)%(L+1))%(L-i%L);
-
 }
 
 /*
-This function returns the energy of one atom.
+This function returns the energy of one atom i,j.
 */
 //not returning -J, so remember to scale back
 float atom_energy(mat spin_matrix, int i, int j, int L){
@@ -36,11 +35,23 @@ float atom_energy(mat spin_matrix, int i, int j, int L){
 }
 
 
+/*
+This function returns the energy from a point i,j to it's neighbours and again
+from the neighbours to itself it the neigbour is not a PBC point. This is needed
+to calculate the DeltaE if we flip a spin.
+*/
 
-float local_energy(mat spin_matrix,int i, int j){
-  float deltaE = 0;
-  deltaE = atom_energy(spin_matrix,i,j);
+float local_energy(mat spin_matrix,int i, int j, int L){
+  float energy = atom_energy(spin_matrix,i,j,L)
 
+  +spin_matrix(PBC_index(i,L),PBC_index(j,L))*(
+    spin_matrix(PBC_index(i+1,L),PBC_index(j,L))* ((i+1-i%(L-1))%(L))
+    +spin_matrix(PBC_index(i,L),PBC_index(j+1,L))* ((j+1-j%(L-1))%(L))
+
+    +spin_matrix(PBC_index(i-1,L),PBC_index(j,L)) * ((1+i-i%L)%(i+1))
+    +spin_matrix(PBC_index(i,L),PBC_index(j-1,L)) * ((1+j-j%L)%(j+1)));
+
+  return energy;
 }
 
 
@@ -81,7 +92,7 @@ int random_index(int start, int finish){
 }
 
 
-mat initialize_matrix(int L){
+mat initialize_matrix_random(int L){
   mat spin_matrix = zeros(L,L);
 
   for(int i=0;i<L;i++){
@@ -92,10 +103,8 @@ mat initialize_matrix(int L){
   return spin_matrix;
 }
 
-mat flip_random_spin(mat spin_matrix,int L){
-  int i = random_index(0,L-1);
-  int j = random_index(0,L-1);
-  spin_matrix(i,j) = minus_one_or_one();
+mat initialize_matrix_ordered(int L){
+  mat spin_matrix = ones(L,L);
   return spin_matrix;
 }
 
@@ -103,19 +112,23 @@ mat flip_random_spin(mat spin_matrix,int L){
 float MC_step(mat spin_matrix,int L, float J){
   float T = 1.2; float k = 1;
   float beta = k*T;
+
   float r = random_between_zero_and_one();
+  int i = random_index(0,L-1);
+  int j = random_index(0,L-1);
 
-  float E_j = system_energy(spin_matrix,L,J);
+  float E_j = local_energy(spin_matrix,i,j,L);
+  spin_matrix(i,j) = minus_one_or_one();
 
-  spin_matrix = flip_random_spin(spin_matrix,L);
-  E_i = system_energy(spin_matrix,L,J);
+  float deltaE = local_energy(spin_matrix,i,j,L)-E_j;
+  cout << deltaE << endl;
 
-  float deltaE = E_i-E_j;
 
-  if(r<=exp(-beta*deltaE)){
-    E_i = E_i+deltaE;
-  }
-  return deltaE;
+  //if(r<=exp(-beta*deltaE)){
+  //  E_i = E_i+deltaE;
+  //}
+  return 1.0;
+  //return deltaE;
 }
 
 /*
@@ -137,11 +150,14 @@ int main()
   srand(clock());
   //srand(1);
 
-  int L  = 2;
+  int L  = 100;
   int J = 1;
-  for(int i=0;i<100;i++){
-    cout << minus_one_or_one() << endl;
-  //mat spin_matrix = initialize_matrix(L);
+  mat spin_matrix = initialize_matrix_random(L);
+
+  for(int i=0;i<1000;i++){
+    MC_step(spin_matrix,L,J);
+    //cout << minus_one_or_one() << endl;
+  //
 
   //cout << atom_energy(spin_matrix,-1,2,L) << endl;
   //spin_matrix.print();
